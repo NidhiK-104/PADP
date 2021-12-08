a915b19220cb243ee01a241f5d18b98bcb78a1a0
NidhiK-104 <73216471+NidhiK-104@users.noreply.github.com>
2021-12-08 09:03:14 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,31 +2,29 @@
 #include <string.h>
 #include <mpi.h>
 #define BUFSIZE 32 
-char m[5][20]={"\nHello","\nCSE","\nRVCE","\n7th Sem","\nHPC"};
+char m[5][20]={"\nHello","\nRVCE","\nCSE","\n7th Sem","\nHPC"};
 int main(int argc,char **argv)
 {
 	char mess[32];
-	int root=0;
+	int root=1;
 	int tag =0;
-	int myRank,numProcs,temp=1;
+	int myRank,numProcs,temp=0;
 	MPI_Init(&argc,&argv);
 	MPI_Status status;
 	MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
 	MPI_Comm_size(MPI_COMM_WORLD,&numProcs);
-	if(myRank==0)
+	if(myRank!=0)
 	{
-		for(temp=1;temp<numProcs;temp++)
-		{
-			strcpy(mess,m[temp-1]);
+			strcpy(mess,m[myRank-1]);
 			MPI_Send(&mess,BUFSIZE,MPI_CHAR,temp,tag,MPI_COMM_WORLD);
-		}	
 	}
 	else
 	{
+	for(root=1; root<numProcs; root++){
 		MPI_Recv(&mess,BUFSIZE,MPI_CHAR,root,tag,MPI_COMM_WORLD,&status);
-		printf("%s is message process of rank %d sent to process of rank %d",mess,myRank,root);
+		printf("%s is message process of rank %d sent to process of rank %d\n",mess,myRank,root);
 	}
-	printf("\n");
+	}
 	MPI_Finalize();
 	return 0;
 }
